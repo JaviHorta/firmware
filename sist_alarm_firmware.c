30b2be9a49f9da758fe9364cf98d38b9fcb6dc00
JaviHorta <javierhorta1999@gmail.com>
2022-12-25 12:48:00 -0500

    Primer commit

---
+++ sist_alarm_firmware.c
@@ -0,0 +1,12 @@
+#include <xparameters.h>
+#include <xgpio_l.h>
+#include <lcd_ip.h>
+#include <xuartlite_l.h>
+#include <mb_interface.h>
+#include <xtmrctr_l.h>
+#include <xintc_l.h>
+
+int main()
+{
+	while(1);
+}
